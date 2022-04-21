ce567ff44a4c050696e290aafef8eefa6ca93355
Lana Septiana <lana.septiana2@gmail.com>
2022-04-21 12:01:32 +0700

    new: konversi category page

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -3,7 +3,7 @@
       data-aos="fade-down"
       >
       <div class="container">
-        <a href="/index.html" class="navbar-brand">
+        <a href="/" class="navbar-brand">
           <img src="images/logo.svg">
         </a>
         <button
@@ -19,11 +19,11 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-              <a href="/index.html" class="nav-link">Home</a>
+            <li class="nav-item">
+              <a href="/" class="nav-link">Home</a>
             </li>
             <li class="nav-item">
-              <a href="/categories.html" class="nav-link">Categories</a>
+              <a href="/categories" class="nav-link">Categories</a>
             </li>
             <li class="nav-item">
               <a href="#" class="nav-link">Rewards</a>
--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
+Route::get('/categories', [App\Http\Controllers\CategoryController::class, 'index']);
 
 Auth::routes();
