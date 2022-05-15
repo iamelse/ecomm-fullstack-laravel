758ff7ecd7b74511b92eac4d881b9cb66aada4c3
Lana Septiana <lanaseptiana2@gmail.com>
2022-05-15 17:13:18 +0700

    new: konversi dashboard transaction page

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\CartController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\DashboardProductController;
+use App\Http\Controllers\DashboardTransactionsController;
 use App\Http\Controllers\DetailController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TestViewController;
@@ -24,10 +25,13 @@ Route::get('/categories', [CategoryController::class, 'index']);
 Route::get('/details/{id}', [DetailController::class, 'index']);
 Route::get('/cart', [CartController::class, 'index']);
 Route::get('/success', [CartController::class, 'success']);
+
 Route::get('/dashboard', [DashboardController::class, 'index']);
 Route::get('/dashboard/products', [DashboardProductController::class, 'index']);
 Route::get('/dashboard/products/create', [DashboardProductController::class, 'create']);
 Route::get('/dashboard/products/details/{id}', [DashboardProductController::class, 'details']);
+
+Route::get('/dashboard/transactions', [DashboardTransactionsController::class, 'index']);
 
 /** Testing View */
 Route::get('/register/success', [TestViewController::class, 'register_success']);
