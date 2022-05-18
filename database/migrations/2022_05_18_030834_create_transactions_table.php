87fc5b047d53a2acc3d8f8813f32faa93eb344c7
Lana Septiana <lana.septiana2@gmail.com>
2022-05-18 10:36:57 +0700

    new: Menambahkan Field Resi dan Shipping Status ke Tabel Transaksi Detail

--- database/migrations/2022_05_18_030834_create_transactions_table.php
+++ database/migrations/2022_05_18_030834_create_transactions_table.php
@@ -19,7 +19,7 @@ class CreateTransactionsTable extends Migration
             $table->integer('insurance_price');
             $table->integer('shipping_price');
             $table->integer('total_price');
-            $table->string('transaction_status'); // UNPAID/PENDING/SHIPPING/SUCCESS/FAILED
+            $table->string('transaction_status'); // UNPAID/PENDING/SUCCESS/FAILED
             $table->string('resi');
             $table->softDeletes();
             $table->timestamps();
