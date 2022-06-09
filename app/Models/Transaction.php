f305c8a0e0adcdf0c4a2236bc3ce5543aabcd34b
Lana Septiana <lana.septiana2@gmail.com>
2022-06-09 09:48:54 +0700

    new: Integrasi Dashboard Page

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Transaction extends Model
 {
@@ -21,4 +22,8 @@ class Transaction extends Model
     protected $hidden = [
 
     ];
+
+    public function user() {
+        return $this->belongsTo(User::class, 'users_id', 'id');
+    }
 }
--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -1,137 +1,165 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
+  <head>
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
     <meta name="description" content="" />
     <meta name="author" content="" />
 
     <title>@yield('title')</title>
 
     @stack('prepend-style')
-        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
-        <link href="{{ asset('style/main.css') }}" rel="stylesheet" />
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
+    <link href="/style/main.css" rel="stylesheet" />
     @stack('addon-style')
-</head>
+  </head>
 
-<body>
+  <body>
     <div class="page-dashboard">
-        <div class="d-flex" id="wrapper" data-aos="fade-right">
-            <!-- Sidebar -->
-            <div class="border-right" id="sidebar-wrapper">
-                <div class="sidebar-heading text-center">
-                    <img src="/images/dashboard-store-logo.svg" alt="" class="my-4" />
-                </div>
-                <div class="list-group list-group-flush">
-                    <a href="/dashboard.html" class="list-group-item list-group-item-action active">Dashboard</a
-            >
+      <div class="d-flex" id="wrapper" data-aos="fade-right">
+        <!-- Sidebar -->
+        <div class="border-right" id="sidebar-wrapper">
+          <div class="sidebar-heading text-center">
+            <img src="/images/dashboard-store-logo.svg" alt="" class="my-4" />
+          </div>
+          <div class="list-group list-group-flush">
             <a
-              href="/dashboard-products.html"
-              class="list-group-item list-group-item-action"
-              >My Products</a
+              href="/dashboard"
+              class="list-group-item list-group-item-action {{ (request()->is('dashboard')) ? 'active' : '' }} "
             >
+              Dashboard
+            </a>
             <a
-              href="/dashboard-transactions.html"
-              class="list-group-item list-group-item-action"
-              >Transactions</a
+              href="/dashboard/products"
+              class="list-group-item list-group-item-action {{ (request()->is('dashboard/products*')) ? 'active' : '' }} "
             >
+              My Products
+            </a>
             <a
-              href="/dashboard-settings.html"
-              class="list-group-item list-group-item-action"
-              >Store Settings</a
+              href="/dashboard/transactions"
+              class="list-group-item list-group-item-action {{ (request()->is('dashboard/transactions*')) ? 'active' : '' }} "
             >
+              Transactions
+            </a>
             <a
-              href="/dashboard-account.html"
-              class="list-group-item list-group-item-action"
-              >My Account</a
+              href="/dashboard/settings"
+              class="list-group-item list-group-item-action {{ (request()->is('dashboard/settings*')) ? 'active' : '' }} "
             >
+              Store Settings
+            </a>
+            <a
+              href="/dashboard/account"
+              class="list-group-item list-group-item-action {{ (request()->is('dashboard/account*')) ? 'active' : '' }} "
+            >
+              My Account
+            </a>
+            <a
+               href="{{ route('logout') }}"
+               onclick="event.preventDefault();
+                        document.getElementById('logout-form').submit();"
+               class="list-group-item list-group-item-action"
+            >
+              Sign Out
+            </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
           </div>
         </div>
-        <!-- /#sidebar-wrapper -->
 
         <!-- Page Content -->
         <div id="page-content-wrapper">
           <nav
-            class="navbar navbar-store navbar-expand-lg navbar-light fixed-top"
+            class="navbar navbar-expand-lg navbar-light navbar-store fixed-top"
             data-aos="fade-down"
           >
-            <button
-              class="btn btn-secondary d-md-none mr-auto mr-2"
-              id="menu-toggle"
-            >
-              &laquo; Menu
-            </button>
-
-            <button
-              class="navbar-toggler"
-              type="button"
-              data-toggle="collapse"
-              data-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav ml-auto d-none d-lg-flex">
-                <li class="nav-item dropdown">
-                  <a
-                    class="nav-link"
-                    href="#"
-                    id="navbarDropdown"
-                    role="button"
-                    data-toggle="dropdown"
-                    aria-haspopup="true"
-                    aria-expanded="false"
-                  >
-                    <img
-                      src="/images/user_pc.png"
-                      alt=""
-                      class="rounded-circle mr-2 profile-picture"
-                    />
-                    Hi, Angga
-                  </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="/index.html">Back to Store</a
+            <div class="container-fluid">
+              <button
+                class="btn btn-secondary d-md-none mr-auto mr-2"
+                id="menu-toggle"
+              >
+                &laquo; Menu
+              </button>
+              <button
+                class="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+              >
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Desktop Menu -->
+                <ul class="navbar-nav d-none d-lg-flex ml-auto">
+                  <li class="nav-item dropdown">
+                    <a
+                        href="#"
+                        class="nav-link"
+                        id="navbarDropdown"
+                        role="button"
+                        data-toggle="dropdown"
                     >
-                    <a class="dropdown-item" href="/dashboard-account.html"
-                      >Settings</a
-                    >
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="/">Logout</a>
-                    </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link d-inline-block mt-2" href="#">
+                        <img
+                            src="{{ asset('images/user_pc.png') }}"
+                            alt=""
+                            class="rounded-circle mr-2 profile-picture"
+                        />
+                        Hi, {{ Auth::user()->name }}
+                    </a>
+                    <div class="dropdown-menu">
+                        <a href="/dashboard" class="dropdown-item">Dashboard</a>
+                        <a href="/dashboard/account" class="dropdown-item">
+                            Settings
+                        </a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            Logout
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                      </div>
+                  </li>
+                  <li class="nav-item">
+                    <a href="cart" class="nav-link d-inline-block mt-2">
+                        @php
+                            $carts = \App\Models\Cart::where('users_id', Auth::user()->id)->count();
+                        @endphp
+                        @if($carts > 0)
+                            <img src="/images/icon-cart-filled.svg" alt="" />
+                            <div class="cart-badge">{{ $carts }}</div>
+                        @else
                             <img src="/images/icon-cart-empty.svg" alt="" />
+                        @endif
+                    </a>
+                  </li>
+                </ul>
+
+                <ul class="navbar-nav d-block d-lg-none">
+                    <li class="nav-item">
+                        <a href="/dashboard" class="nav-link">
+                            Hi, {{ Auth::user()->name }}
                         </a>
                     </li>
-                    </ul>
-                    <!-- Mobile Menu -->
-                    <ul class="navbar-nav d-block d-lg-none mt-3">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                    Hi, Angga
-                  </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link d-inline-block" href="#">
-                    Cart
-                  </a>
-                        </li>
-                    </ul>
-                </div>
-                </nav>
-
-                {{-- Content --}}
-                @yield('content')
-
+                    <li class="nav-item">
+                        <a href="/cart" class="nav-link d-inline-block">
+                            Cart
+                        </a>
+                    </li>
+                </ul>    
+              </div>
             </div>
-            <!-- /#page-content-wrapper -->
+          </nav>
+
+          {{-- Content --}}
+          @yield('content')
+
         </div>
+      </div>
     </div>
 
     <!-- Bootstrap core JavaScript -->
@@ -140,16 +168,14 @@
     <script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <script>
-        AOS.init();
+      AOS.init();
     </script>
-    <!-- Menu Toggle Script -->
     <script>
-        $("#menu-toggle").click(function(e) {
-            e.preventDefault();
-            $("#wrapper").toggleClass("toggled");
-        });
+      $("#menu-toggle").click(function (e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+      });
     </script>
     @stack('addon-script')
-</body>
-
+  </body>
 </html>
